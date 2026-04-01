a8b71e1fcea63bda01f0cbe0785dce4bc5122b17
s0m1k111 <spavel1207@gmail.com>
2026-04-01 09:21:58 +0300

    tep 6: ustom color scheme added

--- app/src/main/java/com/beliakov/student_material_design/MainActivity.kt
+++ app/src/main/java/com/beliakov/student_material_design/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import com.beliakov.student_material_design.data.Student
 import com.beliakov.student_material_design.ui.theme.Student_Material_DesignTheme
 import androidx.compose.foundation.Image
@@ -26,7 +25,12 @@ import com.beliakov.student_material_design.data.students
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.ui.tooling.preview.Preview
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,7 +68,7 @@ fun StudentInformation(
 ) {
     Column(modifier = modifier) {
         Text(
-            text = stringResource(studentAge),
+            text = stringResource(studentName),
             modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small)),
             style = MaterialTheme.typography.displayMedium
         )
@@ -80,22 +84,31 @@ fun StudentItem(
     student: Student,
     modifier: Modifier = Modifier
 ) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(dimensionResource(R.dimen.padding_small))
-    ) {
-        StudentIcon(student.imageResourceId)
-        StudentInformation(student.name, student.age)
+    Card(modifier = modifier) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_small))
+        ) {
+            StudentIcon(student.imageResourceId)
+            StudentInformation(student.name, student.age)
+        }
     }
 }
 @Composable
 fun StudentApp() {
-    LazyColumn {
-        items(students){
-            StudentItem(student = it)
+    Scaffold { it ->
+        LazyColumn(contentPadding = it) {
+            items(students) {
+                StudentItem(
+                    student = it,
+                    modifier = Modifier
+                        .padding(dimensionResource((R.dimen.padding_small)))
+                )
+            }
         }
     }
+
 }
 @Preview
 @Composable
@@ -103,5 +116,4 @@ fun StudentPreview(){
     Student_Material_DesignTheme(darkTheme = false) {
         StudentApp()
     }
-
 }
